99585b73f90bd8e0cd544daf953a2f5c2a6ad7c1
Shubhi Srivastava <91206957+shubhisri15@users.noreply.github.com>
2025-10-14 12:34:27 +0530

    Create Vite App scaffolding using npm, added basic skeletal structure for the final keyframe with all elements, added svg for top sparkle component

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+
+import './App.css'
+import SparkleIcon from './components/SparkleIcon'
+
+function App() {
+  
+  return (
+    <div className='animation-container'>
+      <div className='shapes-container'>
+        
+        <SparkleIcon width='100' height='100' color='white'/>
+        <div className='funnel-shape'>
+          
+        </div>
+      </div>
+      <div className='elevate-text'>ELEVATE</div>
+    </div>
+  )
+}
+
+export default App
