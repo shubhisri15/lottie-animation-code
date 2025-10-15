ea3a77c21ed8ff2667615db78aa3d56c1d79f107
Shubhi Srivastava <91206957+shubhisri15@users.noreply.github.com>
2025-10-15 10:47:16 +0530

    added a replay button to retrigger the animation (by default animation gets triggered on page reload)

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,26 @@
 import './App.css'
 import FunnelIcon from './components/FunnelIcon'
 import SparkleIcon from './components/SparkleIcon'
+import { useState } from 'react'
 
 function App() {
+
+  const [key, setKey] = useState(0)
   
   return (
-    <div className='animation-container'>
-      <div className='shapes-container'>
-        <div className='sparkle'>
-          <SparkleIcon width='150' height='150' color='white'/>
-        </div> 
-        <FunnelIcon />
+    <div className='animation-root' key={key}>
+      <div className='animation-container'>
+        <div className='shapes-container'>
+          <div className='sparkle'>
+            <SparkleIcon width='150' height='150' color='white'/>
+          </div> 
+          <FunnelIcon />
+        </div>
+        <div className='elevate-text'>ELEVATE</div>
       </div>
-      <div className='elevate-text'>ELEVATE</div>
+      <button onClick={() => setKey(k => k + 1)} className='replay-btn'>Replay</button>
     </div>
+    
   )
 }
 
